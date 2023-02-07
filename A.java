8fd71f5bc66578c95fe8dc8c7b666f138f1bd8d5
Manikkavasaki <124652992+Manikkavasaki@users.noreply.github.com>
2023-02-07 04:54:54 +0000

    Basic Java

---
+++ A.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class A 
+{
+public static void main(String args[])
+{
+     Scanner a=new Scanner(System.in);
+	 System.out.println("Enter the number");
+	 int x=a.nextInt();
+	 if(x%2==0)
+	 {
+		 System.out.println(x+ "is an even integer");
+	 }
+	 else
+	 {
+		 System.out.println(x+ "is an odd integer");
+	 }
+	 
+	 
+  }
+  }
